fd7d9fe13216652095fd5dd143e57d800631125b
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-11-13 18:08:26 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/kt_convertimageurltobitmapobjectucoil/WelcomeFragment.kt
+++ app/src/main/java/com/rujirakongsomran/kt_convertimageurltobitmapobjectucoil/WelcomeFragment.kt
@@ -1,10 +1,19 @@
 package com.rujirakongsomran.kt_convertimageurltobitmapobjectucoil
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import coil.ImageLoader
+import coil.request.ImageRequest
+import coil.request.SuccessResult
+import kotlinx.coroutines.launch
 
 class WelcomeFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +21,21 @@ class WelcomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_welcome, container, false)
+
+        lifecycleScope.launch {
+            Log.d("ImageURLToBitmapObject", getBitmap().toString())
+        }
+        return view
+    }
+
+    private suspend fun getBitmap(): Bitmap {
+        val loading = ImageLoader(requireContext())
+        val request = ImageRequest.Builder(requireContext())
+            .data("https://cdn.pixabay.com/photo/2014/04/07/05/25/gummibarchen-318362_1280.jpg")
+            .build()
+
+        val result: Drawable = (loading.execute(request) as SuccessResult).drawable
+        return (result as BitmapDrawable).bitmap
     }
 }
